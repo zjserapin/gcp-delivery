ab3aa67d48bd4d7d4c1286dfd1d749e0722fb2da
zjserapin <zjserapin@yahoo.com>
2020-02-02 15:14:29 -0500

    change this

--- main.py
+++ main.py
@@ -14,9 +14,6 @@ def name(value):
     val = {"value": value}
     return jsonify(val)
 
-@app.route('/dog')
-def dog():
-    return 'My dogs name is Nova'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
